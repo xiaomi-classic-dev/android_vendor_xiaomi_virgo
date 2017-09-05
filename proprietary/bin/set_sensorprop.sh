55fb8ab76685d880de1c3696554518628813d182
GuaiYiHu <guaiyihu@foxmail.com>
2017-09-06 00:18:16 +0800

    virgo: multihal: do not change sensor name
    Change-Id: I37ee59fac562a785d30ef08b3bf1784a2048b313

--- proprietary/bin/set_sensorprop.sh
+++ proprietary/bin/set_sensorprop.sh
@@ -6,9 +6,6 @@ export PATH
 if [ $(getprop ro.boot.hwversion | grep -e 5[0-9]) ]; then
     /system/bin/log -p e -t "SensorSelect" "Device is X5, set HAL to st_mve"
 
-    # help the sensorservice to load the correct HAL
-    setprop ro.hardware.sensors st_mve
-
     # delay sensorservice to wait for hardware being ready
     setprop system_init.startsensorservice 0
     if [ $(getprop ro.boot.hwversion | grep -e 5[0-9]) ]; then
