3ff1ec103f7056ab3dc3b8da34d1ea32c9167a50
GuaiYiHu <guaiyihu@foxmail.com>
2017-09-26 16:14:02 +0800

    virgo: add sony framework
     * kang from xosp
    
    Change-Id: I19e85f88a641b64b427e3304dfac26ab5211b931

--- virgo-vendor.mk
+++ virgo-vendor.mk
@@ -66,12 +66,14 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/virgo/proprietary/etc/firmware/wcnss.b08:system/etc/firmware/wcnss.b08 \
     vendor/xiaomi/virgo/proprietary/etc/firmware/wcnss.b09:system/etc/firmware/wcnss.b09 \
     vendor/xiaomi/virgo/proprietary/etc/firmware/wcnss.mdt:system/etc/firmware/wcnss.mdt \
+    vendor/xiaomi/virgo/proprietary/etc/permissions/com.sony.device.xml:system/etc/permissions/com.sony.device.xml \
     vendor/xiaomi/virgo/proprietary/etc/permissions/qcnvitems.xml:system/etc/permissions/qcnvitems.xml \
     vendor/xiaomi/virgo/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
     vendor/xiaomi/virgo/proprietary/etc/sdm_200_HOG3x3_Grid3x3_bin5_noproj_zero_reduced.bin.pca:system/etc/sdm_200_HOG3x3_Grid3x3_bin5_noproj_zero_reduced.bin.pca \
     vendor/xiaomi/virgo/proprietary/etc/sdm_200_HOG3x3_Grid3x3_bin5_noproj_zero_reduced.bin:system/etc/sdm_200_HOG3x3_Grid3x3_bin5_noproj_zero_reduced.bin \
     vendor/xiaomi/virgo/proprietary/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     vendor/xiaomi/virgo/proprietary/etc/sensor_diag.cfg:system/etc/sensor_diag.cfg \
+    vendor/xiaomi/virgo/proprietary/framework/com.sony.device.jar:system/framework/com.sony.device.jar \
     vendor/xiaomi/virgo/proprietary/framework/qcnvitems.jar:system/framework/qcnvitems.jar \
     vendor/xiaomi/virgo/proprietary/framework/qcrilhook.jar:system/framework/qcrilhook.jar \
     vendor/xiaomi/virgo/proprietary/framework/qti-telephony-common.jar:system/framework/qti-telephony-common.jar \
