d0e82a4fa255d1fbf0a0c5c2588906202741f996
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-11 23:46:02 +0800

    virgo: Remove audio effect conf
    Change-Id: I75eb65414b8075f7ee0d421f8754f7cb236933be

--- virgo-vendor.mk
+++ virgo-vendor.mk
@@ -117,7 +117,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/virgo/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/xiaomi/virgo/proprietary/vendor/lib/libsensors.msm8974.so:system/vendor/lib/libsensors.msm8974.so \
     vendor/xiaomi/virgo/proprietary/lib/hw/audio.primary.msm8974.so:system/lib/hw/audio.primary.msm8974.so \
-    vendor/xiaomi/virgo/proprietary/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     vendor/xiaomi/virgo/proprietary/vendor/etc/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     vendor/xiaomi/virgo/proprietary/vendor/lib/lib-dplmedia.so:system/vendor/lib/lib-dplmedia.so \
     vendor/xiaomi/virgo/proprietary/vendor/lib/lib-rtpcommon.so:system/vendor/lib/lib-rtpcommon.so \
