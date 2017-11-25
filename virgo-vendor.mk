374890ff561a1b8d53bfe0d816ece8abe76395ef
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-25 21:51:19 +0800

    virgo: Clean up dummy audio_policy.conf
    Change-Id: I659b0e27e78cd62cf2ebc644b78c3db72715d9ec

--- virgo-vendor.mk
+++ virgo-vendor.mk
@@ -110,7 +110,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/virgo/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/xiaomi/virgo/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/xiaomi/virgo/proprietary/vendor/bin/wcnss_service:system/vendor/bin/wcnss_service \
-    vendor/xiaomi/virgo/proprietary/vendor/etc/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     vendor/xiaomi/virgo/proprietary/vendor/lib/lib-dplmedia.so:system/vendor/lib/lib-dplmedia.so \
     vendor/xiaomi/virgo/proprietary/vendor/lib/lib-rtpcommon.so:system/vendor/lib/lib-rtpcommon.so \
     vendor/xiaomi/virgo/proprietary/vendor/lib/lib-rtpcore.so:system/vendor/lib/lib-rtpcore.so \
